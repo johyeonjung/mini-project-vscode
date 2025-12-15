079360f68507782bdbef65ac6b480f8b4f0dd619
unknown <askmoon18@naver.com>
2025-12-15 18:20:34 +0900

    first

---
+++ src/pages/auth/styles.js
@@ -0,0 +1,15 @@
+import { css } from "@emotion/react";
+export const layout = css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+`;
+export const container = css`
+    border-radius: 10px;
+    width: 80%;
+    height: 80%;
+    background-color: #ffffff;
+    box-shadow: 0 0 5px 1px #00000033;
+`;
